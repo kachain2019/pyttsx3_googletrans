64936b59ab3815212c590793cd0bc44ec9b5a249
kachain2019 <kachain2019@gmail.com>
2020-05-01 00:24:21 +0700

    2

--- text.py
+++ text.py
@@ -1,10 +1,10 @@
 import pyttsx3
 from googletrans import Translator
-engine = pyttsx3.init() # object creation
+engine = pyttsx3.init()
 sentence = str(input("Say ... > "))
 
 """ RATE"""
-rate = engine.getProperty('rate')   # getting details of current speaking rate
+rate = engine.getProperty('rate')  
 print (rate)                        # printing current voice rate
 engine.setProperty('rate', 125)     # setting up new voice rate
 
@@ -15,8 +15,8 @@ engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
 """VOICE"""
 voices = engine.getProperty('voices')       #getting details of current voice
-engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-# engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+# engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
 
 engine.say(sentence)
 engine.runAndWait()
